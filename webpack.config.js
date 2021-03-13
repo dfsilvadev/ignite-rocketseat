bea401e1e206ca2564ad3143c23fe746a99489d5
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-13 08:00:54 -0300

    feat: primeiras configurações do webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: [path.join(__dirname, "src/js", "index")],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
