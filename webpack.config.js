0ff18d0ed0b45869a33722fe8920514fdfa5e36e
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-15 18:00:02 -0300

    feat: configurando o typescript para o react

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ReactRefrashWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const path = require("path");
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
