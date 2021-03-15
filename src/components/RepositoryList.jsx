351db9492289be4fc23bcb73e6bb9295ba1231a9
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-15 18:00:45 -0300

    feat: add a class container e testando as configurações de typescript

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -17,11 +17,13 @@ export function RepositoryList() {
   }, []);
 
   return (
-    <section className="repository-list">
-      <h1>Lista de repositórios</h1>
-      <ul>
-        <RepositoryItem repositories={repositories} />
-      </ul>
+    <section className="container">
+      <div className="repository-list">
+        <h1>Lista de repositórios</h1>
+        <ul>
+          <RepositoryItem repositories={repositories} />
+        </ul>
+      </div>
     </section>
   );
 }
